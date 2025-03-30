ffc2c77c724d168339dcd11843530e85d86f2f80
Mario Krstevski <mariokrstevski@hotmail.com>
2025-03-30 23:32:15 +0200

    add new route without middle

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,28 +1,28 @@
-import {
-  clerkMiddleware,
-  createRouteMatcher,
-} from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
+// import {
+//   clerkMiddleware,
+//   createRouteMatcher,
+// } from "@clerk/nextjs/server";
+// import { NextResponse } from "next/server";
 
-const isProtectedRoute = createRouteMatcher(["/(.*)"]);
+// const isProtectedRoute = createRouteMatcher(["/(.*)"]);
 
-export default clerkMiddleware(async (auth, req) => {
-  const { pathname } = req.nextUrl;
+// export default clerkMiddleware(async (auth, req) => {
+//   const { pathname } = req.nextUrl;
 
-  // Redirect from '/' to '/dashboard'
-  if (pathname === "/") {
-    return NextResponse.redirect(new URL("/dashboard", req.url));
-  }
+//   // Redirect from '/' to '/dashboard'
+//   if (pathname === "/") {
+//     return NextResponse.redirect(new URL("/dashboard", req.url));
+//   }
 
-  // Protect all other routes except api routes
-  // if (isProtectedRoute(req) && !pathname.startsWith("/api")) {
-  //   await auth.protect();
-  // }
-});
+//   // Protect all other routes except api routes
+//   // if (isProtectedRoute(req) && !pathname.startsWith("/api")) {
+//   //   await auth.protect();
+//   // }
+// });
 
-export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-  ],
-};
+// export const config = {
+//   matcher: [
+//     // Skip Next.js internals and all static files, unless found in search params
+//     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+//   ],
+// };
