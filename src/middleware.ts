21636649e6d87d23b8ffac9a64ff1bb463082224
Mario Krstevski <mariokrstevski@hotmail.com>
2025-03-30 23:28:50 +0200

    with updated middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,9 +14,9 @@ export default clerkMiddleware(async (auth, req) => {
     return NextResponse.redirect(new URL("/dashboard", req.url));
   }
 
-  // Exclude API routes from protection
-  if (!pathname.startsWith("/api")) {
-    if (isProtectedRoute(req)) await auth.protect();
+  // Protect all other routes except api routes
+  if (isProtectedRoute(req) && !pathname.startsWith("/api")) {
+    await auth.protect();
   }
 });
 
@@ -24,7 +24,5 @@ export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    //exlude api routes from middleware.
-    "/((?!api|trpc).*)",
   ],
 };
